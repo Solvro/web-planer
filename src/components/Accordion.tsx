1864be2dc870b6a650b3a2d0496c0c8b521aff45
Marcin Dolatowski <marcindolatowski123@gmail.com>
2024-08-26 17:52:08 +0200

    feat: add select courses and groups
    * feature/enable selecting courses and groups
    
    * fix/add package-lock
    
    * fix/move components from pages
    
    * feature/add ects count, add styles to class blocks
    
    * refactor: apply PR comments

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,14 +1,17 @@
-import { DummyData, Group } from "@/services/usos/getGroups";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { ExtendedCourse } from "@/pages/createplan";
 
 export const GroupsAccordion = ({
+  registrationName,
   index,
   onClick,
-  ...props
+  courses,
 }: {
+  registrationName: string;
   index: number;
-  onClick: (index: number, group: Group) => void;
-} & DummyData) => {
+  onClick: (id: string) => void;
+  courses: ExtendedCourse[];
+}) => {
   return (
     <div className="max-w-96">
       <Accordion type="single" collapsible className="">
@@ -18,32 +21,32 @@ export const GroupsAccordion = ({
               <div className="w-[50px] h-[50px] rounded-[50px] bg-primary items-center flex justify-center font-bold">
                 {index + 1}
               </div>
-              {props.registration.name}
+              {registrationName}
             </div>
           </AccordionTrigger>
           <AccordionContent>
-            {props.courses.map((course) => (
-              <div key={course.course.id}>
-                {course.groups.map((group) => (
-                  <div
-                    key={group.name}
-                    className="grid grid-cols-[1fr_4fr_1fr] gap-4 justify-between break-all items-center py-2 text-base hover:bg-stone-300 hover:cursor-pointer p-4 "
-                  >
-                    <div className="w-[50px] h-[50px] rounded-[50px] bg-secondary items-center flex justify-center">
-                      {group.type.slice(0, 1).toUpperCase()}
+            {courses.map(
+              (course) =>
+                course.registrationName === registrationName && (
+                  <div key={course.name}>
+                    <div className="grid grid-cols-[1fr_4fr_1fr] gap-4 justify-between break-all items-center py-2 text-base hover:bg-stone-300 hover:cursor-pointer p-4 ">
+                      <div className="w-[50px] h-[50px] rounded-[50px] bg-secondary items-center flex justify-center">
+                        {course.name.slice(0, 1).toUpperCase()}
+                      </div>
+                      <label htmlFor={`${index}-${course.name}`} className="hover:cursor-pointer">
+                        {course.name}
+                      </label>
+                      <input
+                        id={`${index}-${course.name}`}
+                        type="checkbox"
+                        onChange={() => onClick(course.name)}
+                        className="w-6 h-6 justify-self-end cursor-pointer"
+                        checked={course.isChecked}
+                      />
                     </div>
-                    <div>{group.name}</div>
-                    <input
-                      id={`${index}-${group.name}`}
-                      type="checkbox"
-                      onChange={() => onClick(index, group)}
-                      className="w-6 h-6 justify-self-end cursor-pointer"
-                      checked={group.isChecked}
-                    />
                   </div>
-                ))}
-              </div>
-            ))}
+                )
+            )}
           </AccordionContent>
         </AccordionItem>
       </Accordion>
---
+++ src/components/selectGroups.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { GroupsAccordion } from "@/components/Accordion";
+import { Registration } from "@/lib/types";
+import { ExtendedCourse } from "../pages/createplan";
+
+export const SelectGroups = ({
+  registrations,
+  courses,
+  checkCourse,
+}: {
+  registrations: Registration[];
+  courses: ExtendedCourse[];
+  checkCourse: (id: string) => void;
+}) => {
+  return (
+    <div className="overflow-auto">
+      <h1 className="text-2xl font-semibold p-4 ">Wybierz odpowiednią turę:</h1>
+      {registrations.map((registration, index) => (
+        <GroupsAccordion
+          key={registration.name}
+          index={index}
+          registrationName={registration.name}
+          onClick={checkCourse}
+          courses={courses}
+        />
+      ))}
+    </div>
+  );
+};
