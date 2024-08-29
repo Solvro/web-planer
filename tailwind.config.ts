a4c12a70478b121ce1def6508104d2d3937106c0
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2024-08-29 18:38:42 +0200

    fix: fix plan styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config = {
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
     "./src/**/*.{ts,tsx}",
-    '*.{html,js}',
+    "*.{html,js}",
   ],
   prefix: "",
   theme: {
@@ -20,7 +20,6 @@ const config = {
     },
     extend: {
       colors: {
-        // START Do wybrania i wywalenia
         solvrodark: "#152959",
         solvrolight: "#132550",
         solvroshadow: "#2c3e6a",
@@ -33,7 +32,6 @@ const config = {
         mainbutton6: "rgb(60, 90, 150)",
         mainbutton7: "rgb(40, 70, 130)",
         solvrolightshadow: "#737f9b",
-        // END
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -78,77 +76,103 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       transitionDuration: {
-        '2000': '2000ms',
-        '5000': '5000ms',
-        '10000': '10000ms',
+        "2000": "2000ms",
+        "5000": "5000ms",
+        "10000": "10000ms",
       },
       keyframes: {
-        'custom-bounce': {
-          '0%, 100%': {
-            transform: 'translateY(-10%)',
-            'animation-timing-function': 'cubic-bezier(0.8, 0, 1, 1)',
+        "custom-bounce": {
+          "0%, 100%": {
+            transform: "translateY(-10%)",
+            "animation-timing-function": "cubic-bezier(0.8, 0, 1, 1)",
           },
-          '50%': {
-            transform: 'translateY(0)',
-            'animation-timing-function': 'cubic-bezier(0, 0, 0.2, 1)',
+          "50%": {
+            transform: "translateY(0)",
+            "animation-timing-function": "cubic-bezier(0, 0, 0.2, 1)",
           },
         },
         wave: {
-          '0%': { transform: 'rotate(0.0deg)' },
-          '10%': { transform: 'rotate(14deg)' },
-          '20%': { transform: 'rotate(-8deg)' },
-          '30%': { transform: 'rotate(14deg)' },
-          '40%': { transform: 'rotate(-4deg)' },
-          '50%': { transform: 'rotate(10.0deg)' },
-          '60%': { transform: 'rotate(0.0deg)' },
-          '100%': { transform: 'rotate(0.0deg)' },
+          "0%": { transform: "rotate(0.0deg)" },
+          "10%": { transform: "rotate(14deg)" },
+          "20%": { transform: "rotate(-8deg)" },
+          "30%": { transform: "rotate(14deg)" },
+          "40%": { transform: "rotate(-4deg)" },
+          "50%": { transform: "rotate(10.0deg)" },
+          "60%": { transform: "rotate(0.0deg)" },
+          "100%": { transform: "rotate(0.0deg)" },
         },
         flip: {
-          '0%': { transform: 'rotateY(0deg)' },
-          '10%': { transform: 'rotateY(15deg)' },
-          '25%': { transform: 'rotateY(30deg)' },
-          '50%': { transform: 'rotateY(0deg)' },
-          '60%': { transform: 'rotateY(-15deg)' },
-          '75%': { transform: 'rotateY(-30deg)' },
-          '100%': { transform: 'rotateY(360deg)' }
+          "0%": { transform: "rotateY(0deg)" },
+          "10%": { transform: "rotateY(15deg)" },
+          "25%": { transform: "rotateY(30deg)" },
+          "50%": { transform: "rotateY(0deg)" },
+          "60%": { transform: "rotateY(-15deg)" },
+          "75%": { transform: "rotateY(-30deg)" },
+          "100%": { transform: "rotateY(360deg)" },
         },
-        'move-top': {
-          '0%': { top: '-10%' },       
-          '100%': { top: '28%' },    
+        "move-top": {
+          "0%": { top: "-10%" },
+          "100%": { top: "28%" },
         },
-        'move-bottom': {
-          '0%': { bottom: '-10%' },       
-          '100%': { bottom: '28%' },    
+        "move-bottom": {
+          "0%": { bottom: "-10%" },
+          "100%": { bottom: "28%" },
         },
         open: {
-          '0%': { transform: 'scaleY(0)', opacity: '0' },
-          '100%': { transform: 'scaleY(1)', opacity: '1' },
+          "0%": { transform: "scaleY(0)", opacity: "0" },
+          "100%": { transform: "scaleY(1)", opacity: "1" },
         },
         close: {
-          '0%': { transform: 'scaleY(1)', opacity: '1' },
-          '100%': { transform: 'scaleY(0)', opacity: '0' },
+          "0%": { transform: "scaleY(1)", opacity: "1" },
+          "100%": { transform: "scaleY(0)", opacity: "0" },
         },
-        'slide-down': {
-          '0%': { transform: 'translateY(-20px)', opacity: '0' },
-          '100%': { transform: 'translateY(0)', opacity: '1' },
+        "slide-down": {
+          "0%": { transform: "translateY(-20px)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
         },
-        'fade-in': {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        gradient: {
+          to: { "background-position": "-200% center" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       animation: {
-        'waving-hand': 'flip 1s infinite',
-        open: 'open 0.3s ease-out forwards',
-        close: 'close 0.3s ease-out forwards',
-        'slide-down': 'slide-down 0.3s ease-out forwards',
-        'fade-in': 'fade-in 0.3s ease-in-out forwards',
-        'move-top': 'move-top 0.3s ease-in-out infinite alternate',
-        'move-bottom': 'move-bottom 0.3s ease-in-out infinite alternate',
+        "waving-hand": "flip 1s infinite",
+        open: "open 0.3s ease-out forwards",
+        close: "close 0.3s ease-out forwards",
+        "slide-down": "slide-down 0.3s ease-out forwards",
+        "fade-in": "fade-in 0.3s ease-in-out forwards",
+        "move-top": "move-top 0.3s ease-in-out infinite alternate",
+        "move-bottom": "move-bottom 0.3s ease-in-out infinite alternate",
+        gradient: "gradient 3s linear infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      gridColumnStart: {
+        "13": "13",
+        "14": "14",
+        "15": "15",
+        "16": "16",
+      },
+      gridTemplateColumns: {
+        "14": "repeat(14, minmax(0, 1fr))",
+        "15": "repeat(15, minmax(0, 1fr))",
+        "16": "repeat(16, minmax(0, 1fr))",
+        dayPlan: "repeat(174, 1fr)",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar")],
 } satisfies Config;
 
 export default config;
