75153037afa8e12bc078a0e30814fcf4c53ed587
D0dii <marcindolatowski123@gmail.com>
2024-08-29 17:35:09 +0200

    feat: add saving to local storage

--- src/components/plan.tsx
+++ src/components/plan.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
-export const Plan = ({ name }: { name: string }) => {
+export const Plan = ({ id, name }: { id: number; name: string }) => {
   return (
-    <button className="bg-white p-4 shadow-lg rounded-lg w-[200px] h-[200px] text-left">
+    <Link
+      href={`createplan/${id}`}
+      className="bg-white p-4 shadow-lg rounded-lg w-[200px] h-[200px] text-left"
+    >
       <div className="font-semibold text-xl">{name}</div>
-      <div className="mt-2 text-gray-600">
-        Kliknij w plan, aby wyświetlić szczegóły
-      </div>
-    </button>
+      <div className="mt-2 text-gray-600">Kliknij w plan, aby wyświetlić szczegóły</div>
+    </Link>
   );
 };
