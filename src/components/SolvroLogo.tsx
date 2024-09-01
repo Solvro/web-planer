f4e9fccf983c6a2f9fd532c97b1b72061c4ce771
olekszczepanowski <olekszczepanowski04@wp.pl>
2024-09-01 22:31:27 +0200

    fuckup: form work like shit

--- src/components/SolvroLogo.tsx
+++ src/components/SolvroLogo.tsx
@@ -1,15 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
+// eslint-disable-next-line no-restricted-imports
+import logo from "../../public/assets/logo/logo_solvro_mono.png";
+
 export const SolvroLogo = () => {
   return (
     <Link href="/" className="inline-block cursor-pointer">
-      <Image
-        src="/assets/logo/logo_solvro_mono.png"
-        alt="Logo Koła Naukowego Solvro"
-        width={50}
-        height={50}
-      />
+      <Image src={logo} alt="Logo Koła Naukowego Solvro" width={50} />
     </Link>
   );
 };
