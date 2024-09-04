89cdc3e034bfa9789c7baa50ac41b787cfa7d67b
olekszczepanowski <olekszczepanowski04@wp.pl>
2024-09-04 12:12:20 +0200

    feat: seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { Space_Grotesk } from "next/font/google";
+import Head from "next/head";
 import Script from "next/script";
 
+import { Seo } from "@/components/SEO";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import type { UmamiTracker } from "@/types/umami";
@@ -20,6 +22,12 @@ declare global {
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Seo />
       <Script
         defer={true}
         src="https://analytics.solvro.pl/script.js"
--- src/pages/plans.tsx
+++ src/pages/plans.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import { IoMdArrowBack } from "react-icons/io";
 
 import { Plan } from "@/components/Plan";
+import { Seo } from "@/components/SEO";
 import { SolvroLogo } from "@/components/SolvroLogo";
 
 import { planFamily } from "./createplan/[id]";
@@ -38,53 +39,56 @@ const Plans = () => {
   };
 
   return (
-    <div className="flex h-screen flex-col items-stretch">
-      <div className="flex h-20 items-center justify-between bg-mainbutton5">
-        <div className="ml-4 w-1/4 flex-none">
-          <SolvroLogo />
-        </div>
-        <div className="flex flex-grow justify-center">
-          <div className="flex items-center justify-center text-lg font-semibold text-white md:text-3xl">
-            <p className="text-nowrap">Twoje Plany</p>
+    <>
+      <Seo pageTitle="Twoje Plany | Planer" />
+      <div className="flex h-screen flex-col items-stretch">
+        <div className="flex h-20 items-center justify-between bg-mainbutton5">
+          <div className="ml-4 w-1/4 flex-none">
+            <SolvroLogo />
+          </div>
+          <div className="flex flex-grow justify-center">
+            <div className="flex items-center justify-center text-lg font-semibold text-white md:text-3xl">
+              <p className="text-nowrap">Twoje Plany</p>
+            </div>
+          </div>
+          <div className="mr-4 flex w-1/4 justify-end">
+            <Image
+              src="https://github.com/shadcn.png"
+              width={40}
+              height={40}
+              className="rounded-full"
+              alt="Picture of the author"
+            />
           </div>
         </div>
-        <div className="mr-4 flex w-1/4 justify-end">
-          <Image
-            src="https://github.com/shadcn.png"
-            width={40}
-            height={40}
-            className="rounded-full"
-            alt="Picture of the author"
-          />
-        </div>
-      </div>
 
-      <div className="container mx-auto max-h-full flex-1 flex-grow overflow-y-auto bg-gray-200 p-4">
-        <div className="flex flex-wrap items-center justify-center gap-4 sm:justify-start">
-          <button
-            onClick={addNewPlan}
-            className="flex h-[200px] w-[200px] items-center justify-center rounded-lg border-2 border-dashed border-gray-400 p-4 shadow-xl"
-          >
-            <span>Dodaj nowy plan</span>
-          </button>
-          {plans.map((plan) => (
-            <Plan key={plan.id} id={plan.id} name={plan.name} />
-          ))}
+        <div className="container mx-auto max-h-full flex-1 flex-grow overflow-y-auto bg-gray-200 p-4">
+          <div className="flex flex-wrap items-center justify-center gap-4 sm:justify-start">
+            <button
+              onClick={addNewPlan}
+              className="flex h-[200px] w-[200px] items-center justify-center rounded-lg border-2 border-dashed border-gray-400 p-4 shadow-xl"
+            >
+              <span>Dodaj nowy plan</span>
+            </button>
+            {plans.map((plan) => (
+              <Plan key={plan.id} id={plan.id} name={plan.name} />
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div>
-        <div className="flex h-14 flex-row items-center justify-between bg-mainbutton3 text-sm text-white">
-          <Link
-            href="/"
-            className="flex h-full flex-1 cursor-pointer items-center justify-center gap-2 text-center font-semibold transition-all hover:bg-solvroshadow hover:shadow-lg"
-          >
-            <IoMdArrowBack size={20} className="block" />
-            Powrót do głównej strony
-          </Link>
+        <div>
+          <div className="flex h-14 flex-row items-center justify-between bg-mainbutton3 text-sm text-white">
+            <Link
+              href="/"
+              className="flex h-full flex-1 cursor-pointer items-center justify-center gap-2 text-center font-semibold transition-all hover:bg-solvroshadow hover:shadow-lg"
+            >
+              <IoMdArrowBack size={20} className="block" />
+              Powrót do głównej strony
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
