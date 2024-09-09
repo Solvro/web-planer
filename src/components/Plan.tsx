df9b99df9ba88b63644b02405522a5e1889399fe
D0dii <marcindolatowski123@gmail.com>
2024-09-09 10:37:18 +0200

    feat: delete plan

--- src/components/Plan.tsx
+++ src/components/Plan.tsx
@@ -1,16 +1,34 @@
+import { useAtom } from "jotai";
 import Link from "next/link";
 import React from "react";
 
+import { planFamily } from "@/pages/createplan/[id]";
+import { plansIds } from "@/pages/plans";
+
 export const Plan = ({ id, name }: { id: number; name: string }) => {
+  const [plans, setPlans] = useAtom(plansIds);
+
+  const deletePlan = () => {
+    planFamily.remove({ id });
+    setPlans(plans.filter((p) => p.id !== id));
+  };
+  // return (
+  //   <Link
+  //     href={`/createplan/${id}`}
+  //     className="flex h-[200px] w-[200px] flex-col items-center justify-center rounded-lg border-2 border-gray-400 bg-white p-4 text-center shadow-lg"
+  //   >
+  //     <div className="text-xl font-semibold">{name}</div>
+  //     <div className="mt-2 text-gray-600">
+  //       Kliknij w plan, aby wyświetlić szczegóły
+  //     </div>
+  //   </Link>
+  // );
   return (
-    <Link
-      href={`/app/createplan/${id}`}
-      className="flex h-[200px] w-[200px] flex-col items-center justify-center rounded-lg border-2 border-gray-400 bg-white p-4 text-center shadow-lg"
-    >
-      <div className="text-xl font-semibold">{name}</div>
-      <div className="mt-2 text-gray-600">
-        Kliknij w plan, aby wyświetlić szczegóły
-      </div>
-    </Link>
+    <div className="flex justify-between bg-slate-600 text-white">
+      <Link href={`/app/createplan/${id}`}>{name}</Link>
+      <button onClick={deletePlan} className="cursor-pointer">
+        Delete
+      </button>
+    </div>
   );
 };
