b0d7365f7ae5a5ab9d5a974d6b31b901f77acb83
olekszczepanowski <olekszczepanowski04@wp.pl>
2024-09-26 21:01:01 +0200

    feat: session auth test

--- backend/my-adonisjs-app/database/seeders/2_user_seeder.ts
+++ backend/my-adonisjs-app/database/seeders/2_user_seeder.ts
@@ -4,8 +4,8 @@ import { DateTime } from 'luxon'
 export default class extends BaseSeeder {
   async run() {
     await User.createMany([
-      { id: '123', updatedAt: DateTime.now() },
-      { id: '234', updatedAt: DateTime.now() },
+      { id: '123', password: 'aa', updatedAt: DateTime.now() },
+      { id: '234', password: 'bb', updatedAt: DateTime.now() },
     ])
   }
 }
