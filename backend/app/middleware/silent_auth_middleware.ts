c682b656eec915f991a5f09e7cd3f2054c1d67c3
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2024-09-29 17:16:20 +0200

    feat: new structure for repo

--- backend/app/middleware/silent_auth_middleware.ts
+++ backend/app/middleware/silent_auth_middleware.ts
@@ -8,10 +8,7 @@ import type { NextFn } from '@adonisjs/core/types/http'
  * The request continues as usual, even when the user is not logged-in.
  */
 export default class SilentAuthMiddleware {
-  async handle(
-    ctx: HttpContext,
-    next: NextFn,
-  ) {
+  async handle(ctx: HttpContext, next: NextFn) {
     await ctx.auth.check()
 
     return next()
--- backend/start/env.ts
+++ backend/start/env.ts
@@ -34,5 +34,5 @@ export default await Env.create(new URL('../', import.meta.url), {
   | Variables for configuring session package
   |----------------------------------------------------------
   */
-  SESSION_DRIVER: Env.schema.enum(['cookie', 'memory'] as const)
+  SESSION_DRIVER: Env.schema.enum(['cookie', 'memory'] as const),
 })
