eeb70177dad4f7760924ed7db98d0026b81bd1e9
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2024-09-29 17:16:20 +0200

    feat: new structure for repo

--- backend/adonisrc.ts
+++ backend/adonisrc.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     () => import('@adonisjs/cors/cors_provider'),
     () => import('@adonisjs/lucid/database_provider'),
     () => import('@adonisjs/session/session_provider'),
-    () => import('@adonisjs/auth/auth_provider')
+    () => import('@adonisjs/auth/auth_provider'),
   ],
 
   /*
