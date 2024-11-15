2a51d53035e71bd3fbe8f338803e42e317e3e45b
QamarQ <kamilm@you2.pl>
2024-11-15 15:54:37 +0100

    fix: lint

--- frontend/src/app/plans/create/[id]/_components/CreateNewPlanPage.tsx
+++ frontend/src/app/plans/create/[id]/_components/CreateNewPlanPage.tsx
@@ -102,7 +102,7 @@ export function CreateNewPlanPage({ planId }: { planId: string }) {
       }) ?? [];
 
   return (
-    <div className="flex w-full flex-1 flex-col items-center justify-center gap-2 md:flex-row md:items-start py-3">
+    <div className="flex w-full flex-1 flex-col items-center justify-center gap-2 py-3 md:flex-row md:items-start">
       <div className="flex w-full max-w-[350px] flex-col items-center justify-center gap-2 px-2 md:ml-4 md:w-4/12 md:flex-col">
         <div className="flex flex-col justify-start gap-3 md:w-full">
           <div className="flex w-full items-end gap-2">
