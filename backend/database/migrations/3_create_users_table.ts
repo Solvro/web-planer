d11416196e62572e90eb7b5042afc739d24808fd
D0dii <marcindolatowski123@gmail.com>
2024-11-21 00:52:03 +0100

    fix: delete second user migration

--- backend/database/migrations/3_create_users_table.ts
+++ backend/database/migrations/3_create_users_table.ts
@@ -5,10 +5,13 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.string('id').primary()
-      table.string('password')
-      table.timestamp('created_at').defaultTo('NOW()')
-      table.timestamp('updated_at')
+      table.increments('id').notNullable()
+      table.string('full_name').nullable()
+      table.string('email', 254).notNullable().unique()
+      table.string('password').notNullable()
+
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').nullable()
     })
   }
 
