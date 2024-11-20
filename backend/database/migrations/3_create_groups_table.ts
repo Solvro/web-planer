97cfdb78b27599fb355f666771962ec5e61188b3
D0dii <marcindolatowski123@gmail.com>
2024-11-20 14:31:39 +0100

    feat: make scraper faster, update models

--- backend/database/migrations/3_create_groups_table.ts
+++ backend/database/migrations/3_create_groups_table.ts
@@ -5,15 +5,16 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.string('id').primary()
+      table.increments('id')
       table.string('name')
       table.time('start_time')
       table.time('end_time')
       table.string('group')
       table.string('lecturer')
-      table.enum('week', ['', 'TN', 'TP'])
+      table.enum('week', ['-', 'TN', 'TP'])
       table.string('day')
       table.string('type')
+      table.string('url')
       table.string('course_id').references('courses.id').onDelete('CASCADE')
       table.timestamp('created_at').defaultTo('NOW()')
       table.timestamp('updated_at')
---
+++ backend/database/seeders/0_department_seeder.ts
@@ -0,0 +1,39 @@
+import Department from '#models/department'
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+
+export default class extends BaseSeeder {
+  async run() {
+    await Department.createMany([
+      {
+        id: 'W01-ABC-XY-1',
+        name: 'Department of Computer Science',
+        url: 'https://web.usos.pwr.edu.pl/kontroler.php?_action=news/rejestracje/rejJednostki&jed_org_kod=CS',
+      },
+      {
+        id: 'W02-DEF-ZY-2',
+        name: 'Department of Mathematics',
+        url: 'https://web.usos.pwr.edu.pl/kontroler.php?_action=news/rejestracje/rejJednostki&jed_org_kod=MATH',
+      },
+      {
+        id: 'W03-GHI-WX-3',
+        name: 'Department of Physics',
+        url: 'https://web.usos.pwr.edu.pl/kontroler.php?_action=news/rejestracje/rejJednostki&jed_org_kod=PHYS',
+      },
+      {
+        id: 'W04-JKL-VU-4',
+        name: 'Department of Chemistry',
+        url: 'https://web.usos.pwr.edu.pl/kontroler.php?_action=news/rejestracje/rejJednostki&jed_org_kod=CHEM',
+      },
+      {
+        id: 'W05-MNO-TS-5',
+        name: 'Department of Biology',
+        url: 'https://web.usos.pwr.edu.pl/kontroler.php?_action=news/rejestracje/rejJednostki&jed_org_kod=BIO',
+      },
+      {
+        id: 'W06-PQR-RQ-6',
+        name: 'Department of Engineering',
+        url: 'https://web.usos.pwr.edu.pl/kontroler.php?_action=news/rejestracje/rejJednostki&jed_org_kod=ENG',
+      },
+    ])
+  }
+}
