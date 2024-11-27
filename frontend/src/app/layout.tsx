805aba3b84f6b1e2cb32b65a6b72c880552b978e
Kamil Marczak <79667721+qamarq@users.noreply.github.com>
2024-11-27 09:36:13 +0100

    fix: fix to umami fix (#101)
    * fix: fix to umami fix
    
    * fix: lint

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
-import Head from "next/head";
+import Script from "next/script";
 import type React from "react";
 
 import ClientProviders from "@/components/Providers";
@@ -95,17 +95,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pl" suppressHydrationWarning={true} className="scroll-smooth">
-      <Head>
-        <script
-          async={true}
-          defer={true}
-          src="https://analytics.solvro.pl/script.js"
-          data-website-id="ab126a0c-c0ab-401b-bf9d-da652aab69ec"
-          data-domains="planer.solvro.pl"
-        />
-      </Head>
       <ClientProviders>
-        <body className={cn(inter.className, "min-h-screen")}>{children}</body>
+        <body className={cn(inter.className, "min-h-screen")}>
+          {children}
+          <Script
+            async={true}
+            defer={true}
+            src="https://analytics.solvro.pl/script.js"
+            data-website-id="ab126a0c-c0ab-401b-bf9d-da652aab69ec"
+            data-domains="planer.solvro.pl"
+          />
+        </body>
       </ClientProviders>
     </html>
   );
