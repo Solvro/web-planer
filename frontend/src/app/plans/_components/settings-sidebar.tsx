d93535e4b90632302f5f396aaaa343e20590772f
QamarQ <kamilm@you2.pl>
2025-01-17 18:23:09 +0100

    feat: mobile version for settings page

--- frontend/src/app/plans/_components/settings-sidebar.tsx
+++ frontend/src/app/plans/_components/settings-sidebar.tsx
@@ -23,10 +23,7 @@ export function SidebarSettings({
 
   return (
     <nav
-      className={cn(
-        "flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1",
-        className,
-      )}
+      className={cn("flex flex-col space-x-0 lg:space-y-1", className)}
       {...props}
     >
       {items.map((item) => (
