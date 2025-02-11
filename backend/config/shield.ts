a86a5fc86854340ea4772d6cd6a163ea8f04faf9
QamarQ <kamilm@you2.pl>
2025-02-12 00:44:33 +0100

    fix: tmp disable

--- backend/config/shield.ts
+++ backend/config/shield.ts
@@ -16,7 +16,7 @@ const shieldConfig = defineConfig({
    * to learn more
    */
   csrf: {
-    enabled: true,
+    enabled: false,
     exceptRoutes: ["/user/login"],
     enableXsrfCookie: false,
     methods: ["POST", "PUT", "PATCH", "DELETE"],
