c404baa0e2826c9521b5234dca52962c08b1298d
QamarQ <kamilm@you2.pl>
2025-02-16 19:22:47 +0100

    chore(frontend): remove comments

--- frontend/src/app/(homepage)/page.tsx
+++ frontend/src/app/(homepage)/page.tsx
@@ -126,15 +126,6 @@ const JoinUsBlock = async () => {
   } catch {
     return (
       <div className="flex flex-col items-center justify-center gap-3 md:flex-row">
-        {/* <Button
-          className="animate-fade-in-2 opacity-0 [--animation-delay:500ms]"
-          asChild={true}
-        >
-          <Link href="/api/login">
-            <Icons.Lock className="size-4" />
-            Zaloguj się z USOS
-          </Link>
-        </Button> */}
         <Button
           className="animate-fade-in-2 opacity-0 [--animation-delay:500ms]"
           asChild={true}
