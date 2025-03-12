09fcc490c73c64637b3b19cc81a23a5ba85d2d3f
Kamil Marczak <kamilm@you2.pl>
2025-03-12 21:31:31 +0100

    feat: cron setup and tests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,22 @@ import { Elysia } from 'elysia'
 
 import { RouteDepartments } from '../routes/departments'
 import { RouteUsers } from '../routes/user'
+import cron, { Patterns } from '@elysiajs/cron'
 
-const app = new Elysia().get('/', () => 'Hello Elysia')
-app.use(RouteDepartments)
-app.use(RouteUsers)
-app.listen(process.env.PORT || 3000)
+const app = new Elysia()
+  .get('/', () => 'Hello Elysia')
+  .use(RouteDepartments)
+  .use(RouteUsers)
+  .use(
+    cron({
+      name: 'scraper',
+      pattern: Patterns.everyDayAt('01:00'),
+      run() {
+        console.log('Run USOS scraper')
+      },
+    })
+  )
+  .listen(process.env.PORT || 3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
