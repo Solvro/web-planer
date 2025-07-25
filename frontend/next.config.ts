5b3e4169133d260e0a21f18c7b26a839eb140105
Kamil Marczak <kamilm@you2.pl>
2025-07-25 13:32:23 +0200

    test: limits

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -16,9 +16,6 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   output: "standalone",
-  experimental: {
-    browserDebugInfoInTerminal: true,
-  },
 };
 
 export default nextConfig;
