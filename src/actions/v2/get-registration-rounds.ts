ce2bc8f01be504f52eb2b70bb1682ca50266f2ef
Oliwier Popielarczyk <71080644+olipop210@users.noreply.github.com>
2026-05-04 23:11:58 +0200

    feat: get registration rounds action
    add server action for fetching rounds for given registration

---
+++ src/actions/v2/get-registration-rounds.ts
@@ -0,0 +1,40 @@
+import redis from "@/lib/redis";
+import { getOrSetRedis } from "@/lib/redis/get-set";
+import { fetchUsosApi } from "@/lib/usos";
+
+interface UsosRegistrationRound {
+  id: string;
+  name: {
+    pl: string;
+    en: string;
+  };
+  status: "active" | "preparing" | "closed";
+  registration_mode:
+    | "double_for_courses"
+    | "double_for_group_with_ask"
+    | "double_group_manual"
+    | "direct_for__courses_and_groups";
+  start_date: Date;
+  end_date: Date;
+}
+
+export async function getRegistrationRoundsAction(
+  registrationId: string,
+): Promise<UsosRegistrationRound[]> {
+  return getOrSetRedis({
+    redis,
+    key: `usos:registration_rounds:${registrationId}`,
+    ttlSeconds: 60,
+    fetcher: async () => {
+      const data = await fetchUsosApi<UsosRegistrationRound[]>(
+        `registrations/course_registration_rounds`,
+        {
+          registration_id: registrationId,
+          fields: "fields=id|name|status|registration_mode|start_date|end_date",
+        },
+      );
+
+      return data;
+    },
+  });
+}
