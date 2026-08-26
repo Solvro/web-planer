17411d90113da46c337b8cdc73e6fac79e840e6b
Oliwier Popielarczyk <71080644+olipop210@users.noreply.github.com>
2026-08-27 01:35:09 +0200

    chore: format

--- src/db/schema/schedule.ts
+++ src/db/schema/schedule.ts
@@ -1,34 +1,35 @@
-import { json, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
+import { json, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
 import { user } from "./auth";
 
 interface scheduleJsonElement {
-    id: string
+  id: string;
 }
 
 export const schedule = pgTable("schedule", {
-    id: text("id").primaryKey(),
-    name: text("name").notNull(),
-    courses: json("courses").$type<scheduleJsonElement[]>().notNull(),
-    registrations: json("registrations").$type<scheduleJsonElement[]>().notNull(),
-    groups: json("groups").$type<scheduleJsonElement[]>().notNull(),
-    userId: text("user_id")
-        .notNull()
-        .references(() => user.id, { onDelete: "cascade" }),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-    updatedAt: timestamp("updated_at")
-        .defaultNow()
-        .$onUpdate(() => /* @__PURE__ */ new Date())
-        .notNull(),
-})
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  courses: json("courses").$type<scheduleJsonElement[]>().notNull(),
+  registrations: json("registrations").$type<scheduleJsonElement[]>().notNull(),
+  groups: json("groups").$type<scheduleJsonElement[]>().notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => /* @__PURE__ */ new Date())
+    .notNull(),
+});
 
 export const scheduleRelations = relations(schedule, ({ one }) => ({
-    user: one(user, {
-        fields: [schedule.userId],
-        references: [user.id],
-    }),
+  user: one(user, {
+    fields: [schedule.userId],
+    references: [user.id],
+  }),
 }));
 
 export const userScheduleRelations = relations(user, ({ many }) => ({
-    plans: many(schedule),
+  plans: many(schedule),
 }));
